d142f98d24eca379b3fd6dae01b18774072bb9d1
guru <guruprasad8424@gmail.com>
2022-06-16 20:15:42 +0530

    factorial

---
+++ factorial.py
@@ -0,0 +1,20 @@
+def factorial(n):
+    res=1
+    if n==0:
+        return(1)
+    else:
+        for i in range(1,n+1):
+            res=res*i
+        return(res)
+print(factorial(4))
+
+#Using Recursion
+def fact(n):
+    res=1
+    if n==0:
+        return(1)
+    else:
+        return(n*fact(n-1))
+print(fact(5))
+
+#Time complexity O(n)--Linear
